8320345261b1a564a39116a2b43137b964bb8332
fjavaler <conscientiadivinus@gmail.com>
2017-04-25 18:04:49 -0600

    commit

---
+++ CodeExLoginSuccess.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login Successful</title>
+    </head>
+    <body>
+        <?php
+        echo $_SESSION['user'] . " has logged in";
+        ?>
+    </body>
+</html>
--- DataBaseConnection.php
+++ DataBaseConnection.php
@@ -5,4 +5,16 @@ $user = 'root';
 $password = 'dbpass';
 $dbname = 'Library';
 $con = new mysqli($host, $user, $password, $dbname)
-        or die('Could not connect to the database server. '.mysqli_connect_error($con));
+        or die('Could not connect to the database server. ' . mysqli_connect_error($con));
+
+//echo "<h2>We Connected</h2>";
+
+function mysql_fix_string($conn, $string)
+{
+    if (get_magic_quotes_gpc())
+    {
+        $string = stripslashes($string);
+    }
+    $string = htmlentities($string);
+    return $conn->real_escape_string($string);
+}
